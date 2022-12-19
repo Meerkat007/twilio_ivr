87762676cf3d826bf3a66400f9096e10380683ea
Yuan <yuanji.yj@gmail.com>
2022-12-18 19:26:42 -0500

    set cors

--- queryData.js
+++ queryData.js
@@ -16,7 +16,7 @@ function getResponse() {
         statusCode: 200,
         headers: {
             'Content-Type': 'application/json',
-            "Access-Control-Allow-Origin": "http://localhost:3000",
+            "Access-Control-Allow-Origin": "*",
         },
         body: JSON.stringify(getResponseBody())
     }
